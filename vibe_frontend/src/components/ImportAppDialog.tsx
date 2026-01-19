cb8f2e16f4fbb508994b079744fe536a78f60b80
Hardik Hadvani <hardik.hadvani@apexon.com>
2026-01-19 20:27:33 +0530

    fix fe build error

--- vibe_frontend/src/components/ImportAppDialog.tsx
+++ vibe_frontend/src/components/ImportAppDialog.tsx
@@ -492,6 +492,7 @@ export function ImportAppDialog({
                 settings={settings}
                 refreshSettings={refreshSettings}
                 handleRepoSetupComplete={() => undefined}
+                refreshApp={() => undefined}
                 expanded={false}
               />
             ) : (
